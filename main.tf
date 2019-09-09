1367bee9d46d9e2380b3af93cae98a2b3a646748
Ramalingam S <magnilamars@gmail.com>
2019-09-09 16:55:29 +0530

    + varunrocks6 : add local-exec to provision ec2 via ansible

--- main.tf
+++ main.tf
@@ -1,34 +1,22 @@
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "test"
+}
 resource "aws_instance" "test_box" {
-  ami = "ami-020ca1ee6a0f716a9"
-  instance_type = "t2.micro"
+  ami               = "ami-020ca1ee6a0f716a9"
+  key_name           = "terraform_keypair"
+  instance_type     = "t2.micro"
   availability_zone = "ap-south-1a"
-  user_data = <<EOF
-                  #!/bin/bash
-                  apt-get install python
-                EOF
-
-  //  connection {
-//    host = self.public_ip
-//    user = "ubuntu"
-//  }
-
-  provisioner "ansible" {
-    plays {
-      playbook {
-        file_path = "master-playbook.yml"
-      }
-      hosts = [aws_instance.test_box.public_ip]
-
-      remote {
-        use_sudo = true
-        skip_install = false
-        skip_cleanup = false
-        install_version = ""
-        local_installer_path = ""
-        remote_installer_directory = "/tmp"
-        bootstrap_directory = "/tmp"
-      }
-    }
+  user_data         = <<EOF
+    #!/bin/bash
+    apt-get install python -y
+  EOF
+  tags = {
+    Name = "ec2-TF-Ansible"
+  }
+  provisioner "local-exec" {
+    command = "sleep 30 && ansible-playbook -i ${self.public_ip}, master-playbook.yml --key-file=./terraform_keypair.pem -u ubuntu"
   }
 }
 
