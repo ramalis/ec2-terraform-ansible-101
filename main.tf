32cf42dd201801b43b8b031e0f12b9bf4b109c85
Ramalingam S <magnilamars@gmail.com>
2019-09-10 11:19:38 +0530

    + varunrocks6 : Adding s3 bucket and dynomo DB for remote state file and locking

--- main.tf
+++ main.tf
@@ -29,5 +29,28 @@ resource "aws_instance" "terraform-ansible-testbox" {
     command = "sleep 30 && ansible-playbook -i ${self.public_ip}, master-playbook.yml --key-file=./${var.keyname} -u ${var.username}"
   }
 }
+# create a dynamodb table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags   = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
+terraform {
+  backend "s3" {
+    bucket         = "my-tftest-s3bucket"
+    encrypt        = true
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock-dynamo"
+  }
+}
 
 
