ba54673d259caf9742e1ffa6f71d30a0e40e9b00
Ramalingam S <magnilamars@gmail.com>
2019-09-11 00:14:31 +0530

    + varunrocks6: changing to latest tf syntax

--- main.tf
+++ main.tf
@@ -9,18 +9,18 @@ variable "path-to-public-key" {}
 data "aws_availability_zones" "azone" {}
 
 provider "aws" {
-  region  = "${var.region}"
-  profile = "${var.profile}"
+  region  = var.region
+  profile = var.profile
 }
 resource "aws_key_pair" "myssh_key" {
   key_name   = var.keyname
   public_key = file(var.path-to-public-key)
 }
 resource "aws_instance" "terraform-ansible-testbox" {
-  ami                         = "${var.ami}"
-  key_name                    = "${var.keyname}"
-  instance_type               = "${var.ec2type}"
-  availability_zone           = "${data.aws_availability_zones.azone.names[0]}"
+  ami                         = var.ami
+  key_name                    = var.keyname
+  instance_type               = var.ec2type
+  availability_zone           = data.aws_availability_zones.azone.names[0]
   associate_public_ip_address = true
   tags = {
     Name = "Terraform-Ansible-Testbox"
@@ -52,5 +52,3 @@ resource "aws_instance" "terraform-ansible-testbox" {
 #     dynamodb_table = "terraform-state-lock-dynamo"
 #   }
 # }
-
-
