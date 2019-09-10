f2255af2899c4d5df120ae81f990a7395605f6ca
Ramalingam S <magnilamars@gmail.com>
2019-09-10 17:20:31 +0530

    +varunrocks6: commented s3 state backend

--- main.tf
+++ main.tf
@@ -30,27 +30,27 @@ resource "aws_instance" "terraform-ansible-testbox" {
   }
 }
 # create a dynamodb table for locking the state file
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "terraform-state-lock-dynamo"
-  hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  tags   = {
-    Name = "DynamoDB Terraform State Lock Table"
-  }
-}
-terraform {
-  backend "s3" {
-    bucket         = "my-tftest-s3bucket"
-    encrypt        = true
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock-dynamo"
-  }
-}
+# resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+#   name           = "terraform-state-lock-dynamo"
+#   hash_key       = "LockID"
+#   read_capacity  = 20
+#   write_capacity = 20
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   tags   = {
+#     Name = "DynamoDB Terraform State Lock Table"
+#   }
+# }
+# terraform {
+#   backend "s3" {
+#     bucket         = "my-tftest-s3bucket"
+#     encrypt        = true
+#     key            = "terraform.tfstate"
+#     region         = "ap-south-1"
+#     dynamodb_table = "terraform-state-lock-dynamo"
+#   }
+# }
 
 
